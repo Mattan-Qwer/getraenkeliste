fdd8f1c0c24aeda376b3cc12b0b2c4f1b7be6ee0
Matthias Nagl <matthias.e.nagl@fau.de>
2018-03-07 09:18:21 +0100

    Tag 2- Uebertragung auf anderen Rechner

--- runme.py
+++ runme.py
@@ -14,7 +14,7 @@ elif sys.version_info >=(3, 4):
 #CSV-Update
 #Input DB
 dbname = str("mitglieder_db.csv")
-dblines = codecs.open(filename, 'r', encoding='utf-8').readlines()
+dblines = codecs.open(dbname, 'r', encoding='utf-8').readlines()
 
 #Output DB
 outputDb = str("auto_mitglieder_db.csv")
@@ -22,38 +22,59 @@ outFile = codecs.open(outputDb, 'w+' , encoding='utf-8')
 
 #LaTex-Datei erstellen
 #Input
-filename = str("getraenkeliste_vorlage.tex")
-lines = codecs.open(filename, 'r', encoding='utf-8').readlines()
+filename_Input = str("getraenkeliste_vorlage.tex")
+lines = codecs.open(filename_Input, 'r', encoding='utf-8').readlines()
 
 #Output
-filename = str("getraenkeliste.tex")
-lines = codecs.open(filename. 'w+', encoding='utf-8')
+filename_Output = str("getraenkeliste.tex")
+lines = codecs.open(filename_Output, 'w+', encoding='utf-8')
 
 #Farbdefinition
 class Farben(object):
 
 	HEVORHEBEN = '\033[4m'
+	ROT ='\033[31m'
 
-def float_imput(promt, default, decimals=0):
+def float_input(promt, default, decimals=0):
 	input_text =""
 	while True:
 		try:
-			input_text =str_input("{promt} [{default}]".format(promt=promt,default =default))
-#TODO: -Input ordentlich machen
+			input_text =str_input(safe_input("{promt} [{default}]".
+				format(promt=promt,default=default)))
+		 	if(len(input_text)) == 0:
+				return default
+				#TODO: elif Float aber zu viel Nachkomma
+			else:
+				return float(input_text)
+		except ValueError:
+			print(colorize(Farbe.ROT, "EUROBETRAG EINGEBEN. Versuche es nochmal"))
 
 
 
 #####Programm start
+linecounter =0
 for dbline in dblines:
-	print(dbline[0:dbline.index(",")].strip())
-	einzahlung = float_input("Einzahlung:"),0.0,2)
+	linecounter += 1
+	if linecounter>1:
+		print(dbline[0:dbline.index(",")].strip())
+		print(dbline[0:dbline.index(",")].strip())
+		einzahlung = float_input("Einzahlung:",0.0,2)
+		print("%d" %einzahlung)
+
+
+	else:
+		outFile.write(dbline)
+
 
 #TODO: Ausgabe schreiben
+
+
+
 
 #TODO: Nutzer anlegen
 
 
-print(colorized(Farbe.
+print("EoF")
 
 
 
